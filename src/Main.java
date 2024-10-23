d55b8c0c353c1b288385de9bcd1b929b1d181c32
Куликов Валерий Романович <VRKulikov@beeline.ru>
2024-10-23 12:22:56 +0300

    Task-1: Defined condition to resolve adult by age

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte minAge = 5;
+        byte maxAge = 70;
+        byte randomAge = getRandomNumber(minAge, maxAge);
+
+        if (randomAge >= 18) {
+            System.out.println("Если возраст человека равен " + randomAge + ", то он совершеннолетний." + "\n");
+        } else {
+            System.out.println("Если возраст человека равен " + randomAge + ", то он не достиг совершеннолетия, нужно немного подождать." + "\n");
+        }
+    }
+
+    public static byte getRandomNumber(byte min, byte max) {
+        return (byte) (Math.floor((float) Math.random() * (max - min + 1)) + min);
     }
 }
